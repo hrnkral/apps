0edabd3fc00cd5092ac49037358c0fa65e2d480e
Harun Karal <hrnkral@gmail.com>
2024-11-14 23:32:43 +0300

    Add project files.

---
+++ Apps/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+
+namespace Apps 
+{
+
+
+
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            // Array
+            var arrc = new char[] { 'b', 't', 'k' };
+
+            // Array List
+
+            var obj = new ArrayList();
+            obj.Add(10);
+            obj.Add('b');
+
+            // List<T>
+
+            var aryint = new List<int>();
+
+            aryint.Add(10);
+            aryint.Add((int)('b'));
+            aryint.AddRange(new int[] {1,2,5,6});
+
+            foreach (var i in aryint)
+            {
+                Console.WriteLine(i);
+                              
+            }
+
+            Console.ReadKey();
+
+
+
+
+        }
+    }
+    
+
+
+
+}
