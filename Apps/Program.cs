3d32cbe3101367ee95b77cc533a888368a96d853
Harun Karal <hrnkral@gmail.com>
2024-11-15 12:14:04 +0300

    dizi kapasitesi dimanik olarak 2'ye katlayacak sekilde ayarlandi

--- Apps/Program.cs
+++ Apps/Program.cs
@@ -10,6 +10,29 @@ namespace Apps
     {
         private static void Main(string[] args)
         {
+            var arr = new DataStructures
+                .Array
+                .Array<int>();
+
+            arr.Add(63);
+            arr.Add(54);
+            arr.Add(47);
+            arr.Add(34);
+            arr.Add(41);
+
+
+            Console.WriteLine($"{arr.Count}/{arr.Capacity}");
+
+
+            Console.ReadKey();
+
+
+
+
+        }
+
+        private static void basitOrnek()
+        {
             // Array
             var arrc = new char[] { 'b', 't', 'k' };
 
@@ -25,19 +48,13 @@ namespace Apps
 
             aryint.Add(10);
             aryint.Add((int)('b'));
-            aryint.AddRange(new int[] {1,2,5,6});
+            aryint.AddRange(new int[] { 1, 2, 5, 6 });
 
             foreach (var i in aryint)
             {
                 Console.WriteLine(i);
-                              
+
             }
-
-            Console.ReadKey();
-
-
-
-
         }
     }
     
--- DataStructures/Array/Array.cs
+++ DataStructures/Array/Array.cs
@@ -18,6 +18,32 @@ namespace DataStructures.Array
             Count = 0;
         }
 
+        public void Add(T item) 
+        {
+            if (InnerList.Length == Count)
+            {
+                DoubleArray();
+            }
+            InnerList[Count] = item;
+            Count++;
+        }
+
+        private void DoubleArray()
+        {
+            var temp = new T[InnerList.Length * 2];
+            /*
+            for (int i = 0; i < temp.Length; i++) 
+            { 
+                temp[i] = InnerList[i];
+            
+            }
+            */
+
+            System.Array.Copy(InnerList, temp, InnerList.Length);
+            InnerList = temp;
+
+        }
+
         public object Clone()
         {
             throw new NotImplementedException();
